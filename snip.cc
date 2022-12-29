437f56bbedd8d58006e21745c0ed3c6f87170976
Ivan Chien <qyc027@gmail.com>
2022-12-29 17:41:10 +0800

    Add snip.cc

---
+++ snip.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <string>
+#include <set>
+#include <map>
+#include <cstring>
+using namespace std;
+typedef long long ll;
+const int inf = 0x3f3f3f3f;
+const int mod = 1e9 + 7;
+#define N 300050
+
+int main()
+{
+    int _;
+    cin >> _;
+    while (_--) {
+        ll n, m; 
+        
+    }
+    return 0;
+}
+
