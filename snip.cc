a231a39e419ee2a6e81c185e07992a89aa78d151
Ivan Chien <qyc027@gmail.com>
2023-03-03 01:25:46 +0800

    Update snip

--- snip.cc
+++ snip.cc
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const int inf = 0x3f3f3f3f;
-const int mod = 1e9 + 7;
+typedef long long i64;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1e9 + 7;
 #define N 300050
+// #define ONLINE_JUDGE
+// Copy to use:
+#ifndef ONLINE_JUDGE
+#endif /* ONLINE_JUDGE */
 
+const bool __SINGLETON = false;
 void solve() {
 
 }
@@ -13,11 +18,12 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int tt;
-    cin >> tt;
-    while (tt--) {
+    if (__SINGLETON) {
         ::solve();
+    } else {
+        i64 tt;
+        cin >> tt;
+        while (tt--) ::solve();
     }
     return 0;
 }
-
